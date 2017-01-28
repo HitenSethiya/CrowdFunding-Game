c5474314becbeda1173f1cccd3aca7acd86ef351
HitenSethiya <hitensethiya@hotmail.com>
2017-01-28 10:00:20 +0530

    apache deploy left

--- CrowdFundingGame/bulk.py
+++ CrowdFundingGame/bulk.py
@@ -2,16 +2,15 @@ from django.contrib.auth import get_user_model
 import random
 from CrowdFundingGame.models import *
 User = get_user_model()
-users = []
+
 
 for i in range(100):
-    user = User(
+    user = User.objects.create_user(
                 username='user%d' %random.randint(1, 15000) ,
-                password='pbkdf2_sha256$30000$O7fqpkuf0dUD$lx6BdckGtenISV6DNijUgvKHahe/SiCK+77q52S1qDA=',
-
+                password='pitchit',
                 )
-    users.append(user)
-    user.save()
+
+
     print(user)
 
 #User.objects.bulk_create(users)
