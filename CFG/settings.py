076fc7dfdfaf8468bed59c479a65da7303a42a26
hiten <hitensethiya@hotmail.com>
2017-07-20 00:54:17 +0530

    modified settings.py (static)

--- CFG/settings.py
+++ CFG/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 #Heroku required additions
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
